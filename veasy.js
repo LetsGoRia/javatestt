cfc00c57c0702c5f941826fdb30b839299fa60df
Izeria Hargrove <Izeria.Hargrove@gmail.com>
2020-12-02 12:15:24 -0500

    message

---
+++ veasy.js
@@ -0,0 +1,49 @@
+
+console.log("Ria Hargrove's Java Script Submission");
+
+//very easy
+
+var lowerNum = 12;
+var upperNum = 30;
+var theDifference = 18;
+console.log(upperNum - lowerNum);
+console.log("The difference between " + upperNum + " and " + lowerNum + " is " + theDifference);
+
+//easy
+
+var firstName = "Octavious";
+var secondName = "Hamilton";
+let nameDifference = 1;
+console.log("The name " + firstName + " is longer than " + secondName + " by " + nameDifference + " letter.");
+
+// medium
+
+
+
+
+
+
+// hard
+
+const add = 23 + 34;
+if (add) {
+    console.log(add);
+}
+
+const subtract = 5602 - 17000;
+if (subtract) {
+    console.log(subtract);
+}
+const divide = 45 / 9;
+if (divide) {
+    console.log (divide);
+}
+
+const multiply = 90 * 45;
+if (multiply) {
+    console.log(multiply);
+}
+
+
+
+// very hard
